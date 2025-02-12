5e37f308757352bf83083e3ef34aaedcac859989
GBergeron1313 <152572549+GBergeron1313@users.noreply.github.com>
2025-02-11 20:45:31 -0500

    Commit 03
    Modified camera controls to make variables more adjustable
    Added boundaries to the camera

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    private float moveSpeed = 0.05f;
-    private float scrollSpeed = 2f;
+    public float cameraSpeed = 0.05f;
+    public float scrollSpeed = 2f;
 
-    public float speedH = 2.0f;
-    public float speedV = 2.0f;
+    public float rotationSpeed = 2.0f;
 
     private float yaw = 0.0f;
     private float pitch = 0.0f;
 
     private bool rotate = false;
+
+    public float boundrySize = 0;
+    public float ceilingHeight = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -26,12 +28,29 @@ public class Camera : MonoBehaviour
     {
         if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
         {
-            transform.position += moveSpeed * new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+            if(transform.position.x <= boundrySize && transform.position.x >= -boundrySize && transform.position.z <= boundrySize && transform.position.z >= -boundrySize)
+            transform.position += cameraSpeed * new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+            if (transform.position.z > boundrySize)
+                transform.position = new Vector3(transform.position.x, transform.position.y, boundrySize);
+            if (transform.position.z < -boundrySize)
+                transform.position = new Vector3(transform.position.x, transform.position.y, - boundrySize);
+            if (transform.position.x > boundrySize)
+                transform.position = new Vector3(boundrySize, transform.position.y, transform.position.z);
+            if (transform.position.x < -boundrySize)
+                transform.position = new Vector3(-boundrySize, transform.position.y, transform.position.z);
+
         }
 
         if (Input.GetAxis("Mouse ScrollWheel") != 0)
         {
+            if (transform.position.y <= ceilingHeight && transform.position.y >= 0)
             transform.position += scrollSpeed * new Vector3(0, Input.GetAxis("Mouse ScrollWheel"), 0);
+
+            if (transform.position.y > ceilingHeight)
+                transform.position = new Vector3(transform.position.x, ceilingHeight, transform.position.z);
+            if (transform.position.y < 0)
+                transform.position = new Vector3(transform.position.x, 0, transform.position.z);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
@@ -47,8 +66,8 @@ public class Camera : MonoBehaviour
         }
         if (rotate)
         {
-            yaw += speedH * Input.GetAxis("Mouse X");
-            pitch -= speedV * Input.GetAxis("Mouse Y");
+            yaw += rotationSpeed * Input.GetAxis("Mouse X");
+            pitch -= rotationSpeed * Input.GetAxis("Mouse Y");
 
             transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
         }
