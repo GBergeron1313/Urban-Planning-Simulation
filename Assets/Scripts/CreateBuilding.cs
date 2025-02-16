b1796d22b4c7ab1aa27f66723bcfdd7884ffb623
GBergeron1313 <152572549+GBergeron1313@users.noreply.github.com>
2025-02-16 13:52:36 -0500

    Graham Commit 7
    UI button spawns workable buildings

--- Assets/Scripts/CreateBuilding.cs
+++ Assets/Scripts/CreateBuilding.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
-public class Create : MonoBehaviour
+public class CreateBuilding: MonoBehaviour
 {
+    public Camera MainCam;
+    public GameObject MainGrid;
+
+    public GameObject buildingPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,16 @@ public class Create : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void createBuilding()
+    {
+        print("Button Clicked"); 
+        Instantiate(buildingPrefab);
+        buildingPrefab.transform.position = new Vector3(6.0f, 0.5f, 0.0f);
+        buildingPrefab.GetComponent<BuildingScript>().mainCam = MainCam;
+        buildingPrefab.GetComponent<BuildingScript>().mainGrid = MainGrid;
         
     }
 }
