4aaacc260f79443e6182c2e2250e7951beb08d75
Reid Blankenship <171534118+reidblankenship@users.noreply.github.com>
2025-03-07 20:25:48 -0700

    Removed path_renderer from Building.cs

--- Assets/Scripts/Citizens/Building.cs
+++ Assets/Scripts/Citizens/Building.cs
@@ -14,7 +14,6 @@ namespace Citizens
 
         private static List<GameObject> building_objects;
         private static List<Vector3> building_positions = new List<Vector3>();
-        private static LineRenderer path_renderer;
         private static Button road_button;
         public NavMeshAgent[] citizens;
 
@@ -31,27 +30,6 @@ namespace Citizens
         public void AddCitizen(GameObject go_citizen)
         {
             go_citizens.Add(go_citizen);
-        }
-
-        public static void RedrawPaths()
-        {
-            // Bunch of line rendering options so the paths aren't stupid looking
-            // By stupid looking I mean: barely visible, magenta, facing the camera like a doom sprite
-            path_renderer.positionCount = building_objects.Count;
-            path_renderer.transform.rotation = Quaternion.LookRotation(Vector3.down, Vector3.up);
-            path_renderer.alignment = LineAlignment.TransformZ;
-            path_renderer.startWidth = 0.5f;
-            path_renderer.endWidth = 0.5f;
-            path_renderer.material.color = Color.Lerp(Color.gray, Color.blue, 0.0625f);
-            path_renderer.loop = true;
-
-            // without this, Z-fighting occurs and the line starts looking glitchy
-            var y_off = Vector3.up * 0.01f;
-
-            for (int i = 0; i < building_objects.Count; i++)
-            {
-                path_renderer.SetPosition(i, building_objects[i].transform.position + y_off);
-            }
         }
 
         public void TrackPosition(Vector3 tracked)
@@ -97,11 +75,7 @@ namespace Citizens
         {
             name = "Building";
             last_citizen_update = Time.time;
-            road_button ??= GameObject.Find("RoadButton").GetComponent<Button>();
-            road_button.onClick.AddListener(RequestUpdate);
-            road_button.onClick.AddListener(RedrawPaths);
             building_objects ??= FindObjectOfType<GridSystem>().GetBuildings();
-            path_renderer ??= gameObject.AddComponent<LineRenderer>();
         }
 
         // Update is called once per frame
