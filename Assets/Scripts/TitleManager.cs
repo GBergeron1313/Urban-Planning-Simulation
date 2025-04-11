f7776d00f1f808c2ec0d83f8fcc536fa2918f9d5
GBergeron1313 <152572549+GBergeron1313@users.noreply.github.com>
2025-04-11 16:43:24 -0400

    Graham Commit 16
    Added basic title screen

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+
+    public Button play, exit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        play.onClick.AddListener(StartProgram);
+        exit.onClick.AddListener(ExitProgram);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartProgram()
+    {
+        SceneManager.LoadScene("Graham_Test_Scene", LoadSceneMode.Single);
+    }
+
+    void ExitProgram()
+    {
+        Application.Quit();
+    }
+}
